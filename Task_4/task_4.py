b8e950883336fa77230af7c0947cb6f763746e59
Akshadha A <95520592+AksA1210@users.noreply.github.com>
2024-02-22 13:11:45 +0530

    Update task_4.py

--- Task_4/task_4.py
+++ Task_4/task_4.py
@@ -25,12 +25,7 @@ def download_images(parquet_file, output_directory, max_images=10000):
                     f.write(response.content)
                 downloaded_count += 1
 
-                current_time = time.time()
-                if current_time - last_check_time >= check_interval:
-                    cpu_usage = psutil.cpu_percent()
-                    network_usage = psutil.net_io_counters()
-                    print(f"Time: {current_time - start_time} sec | CPU Usage: {cpu_usage}% | Network Usage: {network_usage.bytes_sent} bytes sent, {network_usage.bytes_recv} bytes received")
-                    last_check_time = current_time
+                
         except Exception as e:
             print(f"Error downloading image from {url}: {str(e)}")
     display_images(output_directory)
